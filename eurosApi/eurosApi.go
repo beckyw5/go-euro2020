2f1ac9a31c578b569c04cb351245c122db667d56
Becky Wilson <rebecca.wilson2@sky.uk>
2021-07-09 21:19:14 +0100

    alternative apis

--- eurosApi/eurosApi.go
+++ eurosApi/eurosApi.go
@@ -1,4 +1,5 @@
 package euro2020
 
 const baseURL = "https://api.football-data.org/v2/competitions/EC/matches?season=2021"
-
+//const baseURL = "https://api.football-data.org/v2/competitions/EC/matches"
+//const baseURL = "https://statsapi.web.nhl.com/api/v1/"
